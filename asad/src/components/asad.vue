76e24c2cd6154ccec163acd8be2b1f55b4fac23c
axad03213 <axad03213@gmail.com>
2021-09-12 11:58:54 +0500

    check

--- asad/src/components/asad.vue
+++ asad/src/components/asad.vue
@@ -1,11 +1,30 @@
 <template>
-    <h1>{{ as }}</h1>
+    <h1 v-if="show">{{ one }}</h1>
+    <h1 v-else>{{ two }}</h1>
+    <button v-on:click="check">Add 1</button>
 </template>
 <script>
 export default {
   name : 'asad',
-  props : {
-      as:String
+  data(){
+    return {
+      one: 'asad',
+      two: 'Asad is not',
+      show:true
+    }
+  },
+  methods:{
+    check(){
+      this.show = !this.show
+      //this.counter += 1;
+      /*     
+      if (this.show == false){
+          this.show = true
+      } else {
+          this.show = false
+      }
+      */
+    }
   }
 }
 </script>
