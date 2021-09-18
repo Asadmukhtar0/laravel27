f1f3cff927f0cdf499cbaa0238fcd8eaaf15be03
axad03213 <axad03213@gmail.com>
2021-09-18 11:46:37 +0500

    vue js

--- asad/src/components/asad.vue
+++ asad/src/components/asad.vue
@@ -2,18 +2,34 @@
     <h1 v-if="show">{{ one }}</h1>
     <h1 v-else>{{ two }}</h1>
     <button v-on:click="check">Add 1</button>
+    <ul>
+        <li v-for="list in lists" :key="list.id">
+          {{ list.id }}
+        </li>
+    </ul>
 </template>
 <script>
+//import Vue-axios from 'axios';
+//import Vue from 'vue'
+import axios from 'axios'
 export default {
   name : 'asad',
   data(){
     return {
       one: 'asad',
       two: 'Asad is not',
-      show:true
+      lists:null,
+      show:true,
     }
   },
-  methods:{
+  mounted(){
+      axios.get('http://localhost:8000/api/list').then(response =>{
+				this.lists = response,
+        console.log(this.lists)
+			})
+
+  },
+  methods:{ 
     check(){
       this.show = !this.show
       //this.counter += 1;
